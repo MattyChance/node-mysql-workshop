f5ffeb0841dfb88f9862611e64509dbe565e099d
MattyChance <chenmatty.wang@gmail.com>
2016-10-20 01:28:15 +0000

    fixed a small pb for exercise 3

--- exercise-#.js
+++ exercise-#.js
@@ -35,17 +35,17 @@ Exercise 2: Getting back our data
 2. For each account, console.log a line with the account's ID and email, like this: #1:email@domain.com
 3. Use the colors NPM module with the .bold option to achieve this effect
 */
-var query1 = `SELECT * FROM Account LIMIT 5`; 
+// var query1 = `SELECT * FROM Account LIMIT 5`; 
 
-connection.query(query1, function(err, result) {
-  if (err) {
-    console.log(err.stack, 'haha, sth went wrong');
-  }
-  else {
-    console.log(JSON.parse(JSON.stringify(result)));
-  }
-  connection.end();
-});
+// connection.query(query1, function(err, result) {
+//   if (err) {
+//     console.log(err.stack, 'haha, sth went wrong');
+//   }
+//   else {
+//     console.log(JSON.parse(JSON.stringify(result)));
+//   }
+//   connection.end();
+// });
 
 /*
 Exercise 3: Joining up the data, part 1
@@ -54,25 +54,28 @@ Exercise 3: Joining up the data, part 1
 2. Output one line for each account as in Exercise 4, followed by a listing of all the address book names for that account, one per line
 3. Make the output look nice in any way you like
 */
-// var query2 = `SELECT Account.id, Account.email, AddressBook.name FROM Account
-// LEFT JOIN AddressBook 
-// ON Account.id = AddressBook.accountId
-// `; 
+var query2 = `SELECT a.id, a.email, GROUP_CONCAT(ab.name) AS addressbooks 
+FROM AddressBook ab 
+LEFT JOIN Account a 
+ON a.id = ab.accountId 
+GROUP BY a.id;
+`; 
 
-// connection.query(query2, function(err, result) {
-//   if (err) {
-//     console.log(err.stack, 'haha, sth went wrong');
-//   }
-//   else {
-//     //console.log(JSON.parse(JSON.stringify(result)));
-//     result.forEach(function(account) {
-//       console.log('#' + account.id + ': ' + account.email);
-//       (account.name === null ) ? console.log('--no addressbook--') : console.log(account.name);
-//     });
-//   }
-//   // console.log(result);
-//   connection.end();
-// });
+connection.query(query2, function(err, result) {
+  if (err) {
+    console.log(err.stack, 'haha, sth went wrong');
+  }
+  else {
+    //console.log(JSON.parse(JSON.stringify(result)));
+    result.forEach(function(account) {
+      console.log('#' + account.id + ': ' + account.email);
+      console.log(account.addressbooks);
+      //(account.name === null ) ? console.log('--no addressbook--') : console.log(account.name);
+    });
+  }
+  // console.log(result);
+  connection.end();
+});
 
 /*
 Exercise 4: More about joins...
@@ -87,33 +90,35 @@ The only difference, if an account does not have any address book, print it like
 */
 
 //Challenge:
-// var queryChallenge = `select a.id as accountId, a.email, ab.id as addressbookId, ab.name, e.id as entryId, e.firstName, e.lastName
-// from Account a LEFT OUTER JOIN AddressBook ab ON (a.id = ab.accountId) 
-// LEFT OUTER JOIN Entry e ON (ab.id = e.addressbookId)
-// limit 10
-// `; 
+/*
+var queryChallenge = `select a.id as accountId, a.email, ab.id as addressbookId, ab.name, e.id as entryId, e.firstName, e.lastName
+from Account a LEFT OUTER JOIN AddressBook ab ON (a.id = ab.accountId) 
+LEFT OUTER JOIN Entry e ON (ab.id = e.addressbookId)
+limit 10
+`; 
 
-// connection.query(queryChallenge, function(err, result) {
-//   if (err) {
-//     console.log(err.stack, 'haha, sth went wrong');
-//   }
-//   else {
-//     result.reduce(function (prev, curr, idx, arr) {
-//       if (curr['accountId'] !== prev['accountId']) {
+connection.query(queryChallenge, function(err, result) {
+  if (err) {
+    console.log(err.stack, 'haha, sth went wrong');
+  }
+  else {
+    result.reduce(function (prev, curr, idx, arr) {
+      if (curr['accountId'] !== prev['accountId']) {
         
         
         
-//       }
-//     }, []);
+      }
+    }, []);
     
-//     // var test1 = result.map(function (curr) {
-//     //   var obj = {};
-//     //   obj['accountId'] = curr.accountId;
-//     //   obj['email'] = curr.email;
-//     //   // obj['addressbooks'] = [].push('placeholder');
-//     //   return obj;
-//     //});
-//     console.log(result);
-//   }
-//   connection.end();
-// });
+    // var test1 = result.map(function (curr) {
+    //   var obj = {};
+    //   obj['accountId'] = curr.accountId;
+    //   obj['email'] = curr.email;
+    //   // obj['addressbooks'] = [].push('placeholder');
+    //   return obj;
+    //});
+    console.log(result);
+  }
+  connection.end();
+});
+*/
